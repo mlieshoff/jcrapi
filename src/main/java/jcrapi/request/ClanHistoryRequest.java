e0466e80ae7bb92478101bcc675394a04c6e9720
mlieshoff <michaellieshoff@gmx.de>
2018-03-04 18:55:34 +0100

    [issue66] removed toString in Crawler, implemented days parameter in clan history.

--- src/main/java/jcrapi/request/ClanHistoryRequest.java
+++ src/main/java/jcrapi/request/ClanHistoryRequest.java
@@ -3,6 +3,7 @@ package jcrapi.request;
 import com.google.common.base.Preconditions;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Michael Lieshoff
@@ -11,15 +12,34 @@ public class ClanHistoryRequest extends LimitedRequest {
 
     private final String tag;
 
-    private ClanHistoryRequest(String tag, int limit, List<String> excludes, List<String> includes) {
+    private final Integer days;
+
+    private ClanHistoryRequest(String tag, Integer days, int limit, List<String> excludes, List<String> includes) {
         super(limit, excludes, includes);
         Preconditions.checkNotNull(tag, "tag");
         Preconditions.checkArgument(tag.length() > 0, "tag");
+        if (days != null) {
+            Preconditions.checkArgument(days >= 0, "days");
+        }
         this.tag = tag;
+        this.days = days;
     }
 
     public String getTag() {
         return tag;
+    }
+
+    public Integer getDays() {
+        return days;
+    }
+
+    @Override
+    public Map<String, String> getQueryParameters() {
+        Map<String, String>  parameters = super.getQueryParameters();
+        if (days != null) {
+            parameters.put("days", days.toString());
+        }
+        return parameters;
     }
 
     public static ClanHistoryRequestBuilder builder(String tag) {
@@ -30,13 +50,20 @@ public class ClanHistoryRequest extends LimitedRequest {
 
         private final String tag;
 
+        private Integer days;
+
         public ClanHistoryRequestBuilder(String tag) {
             this.tag = tag;
         }
 
+        public ClanHistoryRequestBuilder days(Integer days) {
+            this.days = days;
+            return getThis();
+        }
+
         @Override
         public ClanHistoryRequest build() {
-            return new ClanHistoryRequest(tag, limit, excludes, keys);
+            return new ClanHistoryRequest(tag, days, limit, excludes, keys);
         }
 
         @Override
